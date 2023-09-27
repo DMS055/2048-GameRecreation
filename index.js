5fcc9ff1fc775aeba91c0349851907608f059240
passagenoir <60157796+DMS055@users.noreply.github.com>
2023-09-27 19:48:03 +0200

    JS Setup

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ const coverScreen = document.querySelector(".cover-screen");
 const result = document.getElementById("result");
 const overText = document.getElementById("over-text");
 
+let matrix,
+	score,
+	isSwiped,
+	touchY,
+	initialY = 0,
+	touchX,
+	initialX = 0,
+	row = 4,
+	columns = 4,
+    swipeDirection;
+    
+let rectLeft = grid.getBoundingClientRect().left;
+let rectTop = grid.getBoundingClientRect().top;
+
+const getXY = (e) => {
+
+}
