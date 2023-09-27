2d3688f18d41c79c3ce0caea3c761be55dee4f25
passagenoir <60157796+DMS055@users.noreply.github.com>
2023-09-27 19:29:23 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Imports
+let grid = document.querySelector(".grid");
+const startButton = document.getElementById("startButton");
+const container = document.querySelector(".container");
+const coverScreen = document.querySelector(".cover-screen");
+const result = document.getElementById("result");
+const overText = document.getElementById("over-text");
+
